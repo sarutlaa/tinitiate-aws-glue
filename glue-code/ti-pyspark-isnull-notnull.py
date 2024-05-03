e4b09bd35c1de5bac53c67000a98596224f9fd6c
Sravya Arutla <141533429+sarutlaa@users.noreply.github.com>
2024-05-03 00:21:05 -0400

    Update ti-pyspark-isnull-notnull.py

--- glue-code/ti-pyspark-isnull-notnull.py
+++ glue-code/ti-pyspark-isnull-notnull.py
@@ -15,7 +15,7 @@ catalog = "awsglue_data_catalog"
 database = "glue_db"
 
 # Load tables from Athena into data frames
-df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="category").toDF()
+df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="categories_csv").toDF()
 
 # Filter for null values in a specific column
 df_null = df.filter(col("categoryname").isNull())
@@ -23,6 +23,18 @@ df_null = df.filter(col("categoryname").isNull())
 # Filter for not null values in a specific column
 df_not_null = df.filter(col("categoryname").isNotNull())
 
-# Show the resulting DataFrames
-df_null.show()
-df_not_null.show()
+# Specify the output path for the S3 bucket
+output_base_path = "s3://ti-author-scripts/ti-author-glue-scripts/ti-glue-pyspark-scripts-outputs/ti-pyspark-null-checks-outputs/"
+
+# Save the resulting DataFrame with null values to the S3 bucket in different formats
+df_null.write.mode("overwrite").option("header", "true").csv(output_base_path + "null/csv/")
+df_null.write.mode("overwrite").json(output_base_path + "null/json/")
+df_null.write.mode("overwrite").parquet(output_base_path + "null/parquet/")
+
+# Save the resulting DataFrame with not null values to the S3 bucket in different formats
+df_not_null.write.mode("overwrite").option("header", "true").csv(output_base_path + "not_null/csv/")
+df_not_null.write.mode("overwrite").json(output_base_path + "not_null/json/")
+df_not_null.write.mode("overwrite").parquet(output_base_path + "not_null/parquet/")
+
+# Log information after saving to S3
+glueContext.get_logger().info("Data with null and not null values successfully written to S3 in CSV, JSON, and Parquet formats.")

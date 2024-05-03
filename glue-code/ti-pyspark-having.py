be902f879e1e07f2ccada1c8cf9863f80a1ab217
Sravya Arutla <141533429+sarutlaa@users.noreply.github.com>
2024-05-02 22:49:35 -0400

    Update ti-pyspark-having.py

--- glue-code/ti-pyspark-having.py
+++ glue-code/ti-pyspark-having.py
@@ -15,7 +15,7 @@ catalog = "awsglue_data_catalog"
 database = "glue_db"
 
 # Load tables from Athena into data frames
-grouped_df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="electric_vechiles").toDF()
+grouped_df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="electric_vehicle_population_data_csv").toDF()
 
 # Group by electric vehicle column and count the occurrences
 result_df = grouped_df.groupBy("make", "model").agg(count("*").alias("count"))
@@ -23,5 +23,17 @@ result_df = grouped_df.groupBy("make", "model").agg(count("*").alias("count"))
 # Apply filter similar to HAVING clause
 result_df_filtered = result_df.filter(result_df["count"] > 1000)
 
-# Show the resulting DataFrame
-result_df_filtered.show()
+# Specify the output path for the S3 bucket
+output_base_path = "s3://ti-author-scripts/ti-author-glue-scripts/ti-glue-pyspark-scripts-outputs/ti-pyspark-having-outputs/"
+
+# Save the filtered DataFrame to S3 bucket in CSV format
+result_df_filtered.write.mode("overwrite").option("header", "true").csv(output_base_path + "csv/")
+
+# Save the filtered DataFrame to S3 bucket in JSON format
+result_df_filtered.write.mode("overwrite").json(output_base_path + "json/")
+
+# Save the filtered DataFrame to S3 bucket in Parquet format
+result_df_filtered.write.mode("overwrite").parquet(output_base_path + "parquet/")
+
+# Log information after saving to S3
+glueContext.get_logger().info("Data successfully written to S3 in CSV, JSON, and Parquet formats.")

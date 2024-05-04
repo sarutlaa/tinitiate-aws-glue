e5eaaa655dcb3fce60aa2285b39d2a9b501483ce
Sravya Arutla <141533429+sarutlaa@users.noreply.github.com>
2024-05-03 22:52:05 -0400

    Update ti-pyspark-select.py

--- glue-code/ti-pyspark-select.py
+++ glue-code/ti-pyspark-select.py
@@ -15,31 +15,32 @@ database = "glue_db"
 
 # Load data from AWS Glue catalog into DynamicFrames and convert them to Spark DataFrames.
 product_df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="products_csv").toDF()
+category_df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="categories_csv").toDF()
 
-# Select specific columns from product table and apply aliases
-product_selected_df = product_df.select(
+# Join the products and categories DataFrames on the categoryid
+joined_df = product_df.join(category_df, product_df.categoryid == category_df.categoryid, "inner").select(
     col("productid").alias("Product ID"),
     col("productname").alias("Product Name"),
-    col("categoryid").alias("Product Category ID"),
-    col("unit_price").alias("Cost Per Unit")
+    category_df.categoryname.alias("Category Name"),
+    col("unit_price").alias("Unit Price")
 )
 
-# Filter products where unit price is greater than 5
-filtered_product_df = product_selected_df.filter(col("Cost Per Unit") > 5)
+# Filter the joined DataFrame where the unit price is greater than 5
+filtered_df = joined_df.filter(col("Unit Price") > 5)
 
 # Specify the output path for the S3 bucket
-output_base_path_csv = "s3://your-bucket-name/your-data-folder/csv/"
-output_base_path_json = "s3://your-bucket-name/your-data-folder/json/"
-output_base_path_parquet = "s3://your-bucket-name/your-data-folder/parquet/"
+output_base_path_csv = "s3://your-bucket-name/csv/"
+output_base_path_json = "s3://your-bucket-name/json/"
+output_base_path_parquet = "s3://your-bucket-name/parquet/"
 
 # Save the filtered DataFrame to the S3 bucket in CSV format
-filtered_product_df.write.mode("overwrite").option("header", "true").csv(output_base_path_csv)
+filtered_df.write.mode("overwrite").option("header", "true").csv(output_base_path_csv)
 
 # Save the filtered DataFrame to the S3 bucket in JSON format
-filtered_product_df.write.mode("overwrite").json(output_base_path_json)
+filtered_df.write.mode("overwrite").json(output_base_path_json)
 
 # Save the filtered DataFrame to the S3 bucket in Parquet format
-filtered_product_df.write.mode("overwrite").parquet(output_base_path_parquet)
+filtered_df.write.mode("overwrite").parquet(output_base_path_parquet)
 
 # Log a completion message to indicate successful writing of data to S3.
 glueContext.get_logger().info("Filtered data successfully written to S3 in CSV, JSON, and Parquet formats.")

b2da035c5fa7ed82360aba77b3af98a14d35b314
Sravya Arutla <141533429+sarutlaa@users.noreply.github.com>
2024-05-05 22:04:17 -0400

    Update ti-pyspark-partitions.py

--- glue-code/ti-pyspark-partitions.py
+++ glue-code/ti-pyspark-partitions.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 
 # Initialize Spark context with log level
 sc = SparkContext()
-sc.setLogLevel("INFO")  # Setting log level for Spark context
+sc.setLogLevel("INFO")  # Setting log level for Spark context to INFO
 
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -14,15 +14,22 @@ catalog = "awsglue_data_catalog"
 database = "glue_db"
 
 # Load the table "electric_vehicles" from the Glue Data Catalog into a DataFrame
-df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="electric_vechiles").toDF()
+df = glueContext.create_dynamic_frame.from_catalog(database=database, table_name="electric_vehicles").toDF()
 
 # Define the partitioning columns
-partition_columns = ["model year", "make"]
+partition_columns = ["model_year", "make"]
 
-# Write the DataFrame partitioned by the specified columns
-df.write.partitionBy(*partition_columns).format("parquet").mode("overwrite").save("s3://ti-p-etl-glue/glue_logs/")
+# Base path for S3 bucket where data will be stored
+s3_base_path = "s3://ti-author-scripts/ti-author-glue-scripts/ti-pyspark-partitions.py"
 
-# Optionally, you can also use other file formats like "orc", "json", etc.
+# Write the DataFrame to S3 in Parquet format, partitioned by specified columns
+df.write.partitionBy(*partition_columns).format("parquet").mode("overwrite").save(s3_base_path + "parquet/")
 
-# Show the DataFrame partitions
+# Write the DataFrame to S3 in JSON format, partitioned by specified columns
+df.write.partitionBy(*partition_columns).format("json").mode("overwrite").save(s3_base_path + "json/")
+
+# Write the DataFrame to S3 in CSV format, partitioned by specified columns
+df.write.partitionBy(*partition_columns).format("csv").mode("overwrite").option("header", "true").save(s3_base_path + "csv/")
+
+# Print the schema of the DataFrame to verify the structure
 df.printSchema()

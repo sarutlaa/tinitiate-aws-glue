7d939125b640cae680ead0920c37e8b88597bb58
Sravya Arutla <141533429+sarutlaa@users.noreply.github.com>
2024-05-12 14:05:57 -0400

    Update ti-pyspark-read-from-json-s3.py

--- glue-code/ti-pyspark-read-from-json-s3.py
+++ glue-code/ti-pyspark-read-from-json-s3.py
@@ -11,7 +11,7 @@ job = Job(glueContext)
 
 # Specify the S3 path to the JSON and gzip-compressed JSON files
 json_path = "s3://ti-author-data/customer-billing/json/"
-# gzip_json_path = "s3://ti-author-data/customer-billing/json-gz/"
+gzip_json_path = "s3://ti-author-data/customer-billing/json-gz/"
 
 # Read the regular JSON file using DynamicFrame
 dynamic_frame_json = glueContext.create_dynamic_frame.from_options(
@@ -20,21 +20,15 @@ dynamic_frame_json = glueContext.create_dynamic_frame.from_options(
     format="json"
 )
 
-# # Read the gzip-compressed JSON file using DynamicFrame
-# dynamic_frame_gzip_json = glueContext.create_dynamic_frame.from_options(
-#     connection_type="s3",
-#     connection_options={"paths": [gzip_json_path]},
-#     format="json",
-#     format_options={"compression": "gzip"}
-# )
+# Read the gzip-compressed JSON file using Spark DataFrame as DynamicFrame doesn't support reads from compressed json file.
+df_gzip_json = spark.read.option("compression", "gzip").json(gzip_json_path)
 
 # Convert DynamicFrames to DataFrames to use Spark SQL functionalities
 df_json = dynamic_frame_json.toDF()
-# df_gzip_json = dynamic_frame_gzip_json.toDF()
 
 # Example processing (simple count)
-print("Count of rows in regular JSON:", df_json.show(5))
-# print("Count of rows in gzip JSON:", df_gzip_json.show(5))
+print("Regular JSON file preview:", df_json.show(5))
+print("Compressed JSON file preview:", df_gzip_json.show(5))
 
 # Initialize and commit the job
 job.init("manual-job-name", {})

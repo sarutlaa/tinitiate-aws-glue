2e68e64c16cff119170c7d50022e982c65fdbb5c
Sravya Arutla <141533429+sarutlaa@users.noreply.github.com>
2024-05-12 13:07:23 -0400

    Update ti-pyspark-write-to-csv.py

--- glue-code/ti-pyspark-write-to-csv.py
+++ glue-code/ti-pyspark-write-to-csv.py
@@ -19,32 +19,35 @@ data = [
 # Convert sample data to DataFrame
 df = sc.parallelize(data).toDF()
 
-# Convert DataFrame to DynamicFrame
-dynamic_df = DynamicFrame.fromDF(df, glueContext, "dynamic_df")
+# Use coalesce to reduce the DataFrame to one partition
+df_single = df.coalesce(1)
+
+# Convert the coalesced DataFrame to DynamicFrame
+dynamic_df_single = DynamicFrame.fromDF(df_single, glueContext, "dynamic_df_single")
 
 # Specify the S3 path for output
-output_dir_csv = "s3://ti-author-scripts/ti-author-glue-scripts/ti-glue-pyspark-scripts-outputs/ti-pyspark-write-to-csv-outputs/"
+output_dir_csv = "s3://ti-author-scripts/ti-author-glue-scripts/ti-glue-pyspark-scripts-outputs/ti-pyspark-write-to-csv-outputs/csv/data_csv.csv"
+output_dir_csv_gz = "s3://ti-author-scripts/ti-author-glue-scripts/ti-glue-pyspark-scripts-outputs/ti-pyspark-write-to-csv-outputs/csv-gz/data_csv_gz.csv.gz"
 
-# Write the data to S3 as CSV
+# Write the data to S3 as a single CSV file
 glueContext.write_dynamic_frame.from_options(
-    frame = dynamic_df,
-    connection_type = "s3",
-    connection_options = {"path": output_dir_csv},
-    format = "csv",
-    format_options = {"writeHeader": True}
+    frame=dynamic_df_single,
+    connection_type="s3",
+    connection_options={"path": output_dir_csv},
+    format="csv",
+    format_options={"writeHeader": True}
 )
 
-# Write the data to S3 as gzip-compressed CSV
+# Write the data to S3 as a single gzip-compressed CSV file
 glueContext.write_dynamic_frame.from_options(
-    frame = dynamic_df,
-    connection_type = "s3",
-    connection_options = {"path": output_dir_csv},
-    format = "csv",
-    format_options = {"writeHeader": True, "compression": "gzip"}
+    frame=dynamic_df_single,
+    connection_type="s3",
+    connection_options={"path": output_dir_csv_gz},
+    format="csv",
+    format_options={"writeHeader": True, "compression": "gzip"}
 )
 
 print("Successfully written the data in S3")
 
 # Commit the job to indicate successful completion
 job.commit()
-

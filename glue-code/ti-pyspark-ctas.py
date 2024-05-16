1db2f5689e4f2d0e74902eef06199722f1623af7
Sravya Arutla <141533429+sarutlaa@users.noreply.github.com>
2024-05-16 19:20:34 -0400

    Update ti-pyspark-ctas.py

--- glue-code/ti-pyspark-ctas.py
+++ glue-code/ti-pyspark-ctas.py
@@ -11,7 +11,9 @@ spark = glueContext.spark_session
 # Load the 'purchase' table from the AWS Glue Data Catalog
 purchase_df = glueContext.create_dynamic_frame.from_catalog(database="glue_db", table_name="purchase").toDF()
 
-# Assuming 'purchase' table has columns: purchase_tnx_id, product_supplier_id, purchase_tnxdate, quantity, invoice_price
+print("Original Table:")
+purchase_df.show()
+
 # Filter purchases where the quantity is greater than a threshold, e.g., 100
 filtered_df = purchase_df.filter(purchase_df["quantity"] > 100)
 
@@ -20,21 +22,20 @@ filtered_df.createOrReplaceTempView("ctas_purchase_table")
 
 # Define the CTAS query to create a new table in the AWS Glue Data Catalog
 ctas_query = """
-CREATE TABLE glue_db.ctas_purchase_table
-USING CSV
+CREATE TABLE glue_db.ctas_filtered_purchase
+USING parquet
 OPTIONS (
-    path 's3://ti-author-scripts/ti-author-glue-scripts/ti-glue-pyspark-scripts-outputs/ctas-output-table/'
+    path 's3://ti-author-scripts/ti-author-glue-scripts/ti-glue-pyspark-scripts-outputs/ti-pyspark-ctas-outputs/'
 )
 AS SELECT * FROM ctas_purchase_table
 """
 
 # Execute the CTAS operation
-result_df = spark.sql(ctas_query)
-print("Sample CTAS Table")
-result_df.show()
+spark.sql(ctas_query)
+print("CTAS operation executed. New table 'ctas_filtered_purchase' created in the Glue Data Catalog.")
 
 # Log the completion of the CTAS operation
-glueContext.get_logger().info("CTAS operation completed and new table 'ctas_purchase_table' created in the Glue Data Catalog.")
+glueContext.get_logger().info("CTAS operation completed and new table 'ctas_filtered_purchase' created in the Glue Data Catalog.")
 
 # Stop the Spark context to free up resources and close the session
 sc.stop()
